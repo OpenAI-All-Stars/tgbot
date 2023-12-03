ce7387375cbe63f8cd9cbcff7420bf5db72ff857
Ilya Chistyakov <ilchistyakov@gmail.com>
2023-12-03 14:55:03 +0300

    fix executor url

--- src/tgbot/settings/prod.py
+++ src/tgbot/settings/prod.py
@@ -3,5 +3,5 @@ from .base import *  # noqa
 
 SQLITE_PATH = 'db.sqlite3'
 OPENAI_MODEL = 'gpt-3.5-turbo-0613'  # example gpt-3.5-turbo-0613, gpt-4
-EXECUTOR_BASE_URL = 'http://executor'
+EXECUTOR_BASE_URL = 'http://executor:8000'
 TELEGRAM_BASE_URL = 'https://api.telegram.org'
