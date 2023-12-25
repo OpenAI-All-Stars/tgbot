658a769045e00154fb1741297476faf66e434501
Ilya Chistyakov <ilchistyakov@gmail.com>
2023-12-25 07:30:45 +0300

    fix invite

--- src/tgbot/repositories/invite.py
+++ src/tgbot/repositories/invite.py
@@ -1,25 +1,26 @@
-import json
 import time
 
 import jwt
 from simple_settings import settings
 
 
-FIRST_SEGMENT = json.dumps({
-  "alg": "HS256",
-  "typ": "JWT",
-})
+ALG = 'HS256'
+FIRST_SEGMENT = jwt.encode(
+    {},
+    settings.SECRET_INVITE,
+    algorithm=ALG,
+).split('.')[0]
 
 
 def generate_code():
-    encoded_jwt = jwt.encode({'t': time.time()}, settings.SECRET_INVITE, algorithm='HS256')
+    encoded_jwt = jwt.encode({'t': time.time()}, settings.SECRET_INVITE, algorithm=ALG)
     parts = encoded_jwt.split('.')
     return '.'.join(parts[1:])
 
 
 def get_payload(code: str) -> str | None:
     try:
-        payload = jwt.decode(f'{FIRST_SEGMENT}.{code}', settings.SECRET_INVITE, algorithms=['HS256'])
+        payload = jwt.decode(f'{FIRST_SEGMENT}.{code}', settings.SECRET_INVITE, algorithms=[ALG])
     except jwt.PyJWTError:
         return None
     return str(payload['t'])
