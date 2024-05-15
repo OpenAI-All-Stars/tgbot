4c6e0c6e8e7a5e44982494fd3c25f8bfe7c123ef
Ilya Chistyakov <ilchistyakov@gmail.com>
2024-05-16 02:26:51 +0700

    new model

--- src/tgbot/settings/prod.py
+++ src/tgbot/settings/prod.py
@@ -2,7 +2,6 @@ from .base import *  # noqa
 
 
 SQLITE_PATH = 'db.sqlite3'
-OPENAI_MODEL = 'gpt-4-1106-preview'  # example gpt-3.5-turbo-0613, gpt-4-1106-preview
 EXECUTOR_BASE_URL = 'http://executor:8000'
 TELEGRAM_BASE_URL = 'https://api.telegram.org'
 
