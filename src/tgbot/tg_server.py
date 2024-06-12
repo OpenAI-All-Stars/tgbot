e054dd99771188fd6794e641bb6fae6c4631e4db
Ilya Chistyakov <ilchistyakov@gmail.com>
2024-06-12 10:57:16 +0300

    fix

--- src/tgbot/tg_server.py
+++ src/tgbot/tg_server.py
@@ -111,8 +111,7 @@ async def send_answer(message: types.Message) -> None:
             answer,
         )
     else:
-        for part in _split_answer(answer):
-            await message.answer(part)
+        await message.answer(answer)
 
 
 async def run() -> None:
@@ -128,34 +127,9 @@ async def run() -> None:
 
 def _split_answer(answer: str) -> Iterator[str]:
     MAX_LEN = 4096
-    while True:
-        if len(answer) < MAX_LEN:
+    while answer:
+        if len(answer) <= MAX_LEN:
             break
-        part, answer = _get_part(answer, MAX_LEN)
+        part = answer[:MAX_LEN]
+        answer = answer[MAX_LEN:]
         yield part
-    if answer:
-        part, _ = _get_part(answer, MAX_LEN)
-        yield part
-
-
-def _get_part(answer: str, max_len: int) -> tuple[str, str]:
-    part = answer[:max_len]
-    answer = answer[max_len:]
-    while True:
-        counts = {
-            '_': 0,
-            '*': 0,
-            '`': 0,
-        }
-        for c in part:
-            if c in counts:
-                counts[c] += 1
-        need_continue = False
-        for k, v in counts.items():
-            if v % 2 != 0:
-                answer = part[-1] + answer
-                part = part[:-1]
-                need_continue = True
-        if not need_continue:
-            break
-    return part, answer
