931b4c9f4bc660b923d2a91d80e3b8ed28f7b2f8
Ilya Chistyakov <ilchistyakov@gmail.com>
2024-07-14 16:54:09 +0300

    fix

--- contrib/migrations/003_refactoring.sql
+++ contrib/migrations/003_refactoring.sql
@@ -2,4 +2,4 @@ ALTER TABLE users DROP COLUMN chat_id;
 
 ALTER TABLE chat_messages ADD COLUMN user_id INTEGER;
 UPDATE chat_messages SET user_id = chat_id;
-ALTER TABLE your_table_name ALTER COLUMN user_id SET REFERENCES users(user_id);
+ALTER TABLE chat_messages ALTER COLUMN user_id SET REFERENCES users(user_id);
