2bee46e3d9e7c647f85c68442df25fcac3d0fcc2
Ilya Chistyakov <ilchistyakov@gmail.com>
2024-07-15 04:43:50 +0300

    fix logging

--- src/tgbot/web_startup.py
+++ src/tgbot/web_startup.py
@@ -1,11 +1,20 @@
 import asyncio
 from contextlib import asynccontextmanager
 
+import sentry_sdk
+from simple_settings import settings
+
 from tgbot import deps, tg_server
 
 
 @asynccontextmanager
 async def lifespan(_):
+    if settings.SENTRY_DSN:
+        sentry_sdk.init(
+            dsn=settings.SENTRY_DSN,
+            traces_sample_rate=1.0,
+            profiles_sample_rate=1.0,
+        )
     async with deps.use_all():
         asyncio.create_task(tg_server.run())
         yield
