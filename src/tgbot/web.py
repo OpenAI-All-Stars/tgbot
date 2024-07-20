5cf1c9a3837c6f661b9fed765dddb694d0c6cf5b
Ilya Chistyakov <ilchistyakov@gmail.com>
2024-07-20 03:26:58 +0300

    add balance notify (#93)
    * add balance notify
    
    * fix

--- src/tgbot/web.py
+++ src/tgbot/web.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from tgbot.deps import tg_bot
 from tgbot.entities.web import AddBalanceRequest
 from tgbot.servicecs import wallet
 from tgbot.web_startup import lifespan
@@ -11,3 +12,7 @@ app = FastAPI(lifespan=lifespan)
 @app.post('/add-balance')
 async def add_balance_handler(r: AddBalanceRequest):
     await wallet.add(r.user_id, r.microdollars)
+    await tg_bot.get().send_message(
+        r.user_id,
+        'Зачислено на счёт ${:.2f}'.format(r.dollars),
+    )
