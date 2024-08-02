7d12ab0dd8603bff3f3b46420aa381201be1f025
Ilya Chistyakov <ilchistyakov@gmail.com>
2024-08-02 07:37:29 +0300

    tune python run

--- src/tgbot/repositories/docker.py
+++ src/tgbot/repositories/docker.py
@@ -8,23 +8,25 @@ import docker
 
 
 IMAGE_TAG = 'tgbot-py310'
+WORK_DIR = '/tmp/tgbot/docker-crumbs'
+DOCKER_FILE = 'py310.dockerfile'
 _executor = ThreadPoolExecutor(max_workers=1)
 
 
-async def run_code(code: str, work_dir: str, timeout: float) -> tuple[str, dict]:
+async def run_code(code: str, timeout: float) -> tuple[str, dict]:
     return await asyncio.get_running_loop().run_in_executor(
         _executor,
         sync_run_code,
-        code, work_dir, timeout,
+        code, timeout,
     )
 
 
-def sync_run_code(code: str, work_dir: str, timeout: float) -> tuple[str, dict]:
+def sync_run_code(code: str,  timeout: float) -> tuple[str, dict]:
     client = docker.from_env()
 
-    if os.path.exists(work_dir):
-        shutil.rmtree(work_dir)
-    os.makedirs(work_dir)
+    if os.path.exists(WORK_DIR):
+        shutil.rmtree(WORK_DIR)
+    os.makedirs(WORK_DIR)
 
     container_limits = {
         # 'cpus': '0.5',
@@ -41,10 +43,11 @@ def sync_run_code(code: str, work_dir: str, timeout: float) -> tuple[str, dict]:
         container = client.containers.run(
             image=IMAGE_TAG,
             command=f'python -c {code}',
-            volumes={work_dir: {'bind': '/app', 'mode': 'rw'}},
+            volumes={WORK_DIR: {'bind': '/app', 'mode': 'rw'}},
             working_dir='/app',
             detach=True,
             network_disabled=True,
+            user=f'{os.getuid()}:{os.getgid()}',
             **container_limits
         )
 
@@ -52,13 +55,13 @@ def sync_run_code(code: str, work_dir: str, timeout: float) -> tuple[str, dict]:
         log = container.logs().decode('utf-8')
         container.remove()
 
-        created_files = os.listdir(work_dir)
+        created_files = os.listdir(WORK_DIR)
         files_content = {}
         for file_name in created_files:
-            with open(os.path.join(work_dir, file_name), 'rb') as file:
+            with open(os.path.join(WORK_DIR, file_name), 'rb') as file:
                 files_content[file_name] = file.read()
     finally:
-        shutil.rmtree(work_dir)
+        shutil.rmtree(WORK_DIR)
 
     return log, files_content
 
@@ -74,6 +77,6 @@ def sync_build():
     client = docker.from_env()
     client.images.build(
         path='.',
-        dockerfile='py310.dockerfile',
+        dockerfile=DOCKER_FILE,
         tag=IMAGE_TAG,
     )
